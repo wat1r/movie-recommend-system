89383d0a184cb4279c248572727bf1f5058d0456
wat1r <cnwangzhou@hotmail.com>
2019-04-30 23:19:31 +0800

    OfflineRecommender

--- recommender/offlineRecommender/src/main/scala/com/frankcooper/offline/OfflineRecommender.scala
+++ recommender/offlineRecommender/src/main/scala/com/frankcooper/offline/OfflineRecommender.scala
@@ -3,6 +3,7 @@ package com.frankcooper.offline
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.recommendation.{ALS, Rating}
 import org.apache.spark.sql.SparkSession
+import org.jblas.DoubleMatrix
 
 /**
   *
@@ -67,6 +68,7 @@ object OfflineRecommender {
   val USER_MAX_RECOMMENDATION = 10
 
   val USER_RECS = "UserRecs"
+  val MOVIE_RECS = "MovieRecs"
 
 
   def main(args: Array[String]): Unit = {
@@ -102,7 +104,7 @@ object OfflineRecommender {
       .load()
       .as[MovieRating]
       .rdd
-      .map(rating => (rating.uid, rating.mid, rating.score))
+      .map(rating => (rating.uid, rating.mid, rating.score)).cache()
 
 
     //用户的数据集 RDD[Int]
@@ -147,8 +149,38 @@ object OfflineRecommender {
       .format("com.mongodb.spark.sql")
       .save()
 
+    //计算电影相似度矩阵
+
+    //获取电影的特征矩阵
+    val movieFeatures = model.productFeatures.map({
+      case (mid, freatures) => (mid, new DoubleMatrix(freatures))
+    })
+
+    val movieRecs = movieFeatures.cartesian(movieFeatures)
+      .filter { case (a, b) => a._1 != b._1 }
+      .map { case (a, b) =>
+        val simScore = this.consinSim(a._2, b._2)
+        (a._1, (b._1, simScore))
+      }.filter(_._2._2 > 0.6)
+      .groupByKey()
+      .map { case (mid, items) =>
+        MovieRecs(mid, items.toList.map(x => Recommendation(x._1, x._2)))
+      }.toDF()
+
+    movieRecs
+      .write
+      .option("uri", mongoConfig.uri)
+      .option("collection", MOVIE_RECS)
+      .mode("overwrite")
+      .format("com.mongodb.spark.sql")
+      .save()
 
     spark.close()
   }
 
+
+  //计算两个电影之间的余弦相似度
+  def consinSim(movie1: DoubleMatrix, movie2: DoubleMatrix): Double = {
+    movie1.dot(movie2) / (movie1.norm2() * movie2.norm2())
+  }
 }
